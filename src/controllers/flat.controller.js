552a863599782577a763801137d90cc38361f353
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-07-08 23:32:40 +0530

    flat controller added

--- src/controllers/flat.controller.js
+++ src/controllers/flat.controller.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
   try {
     let flat = await Flat.create(req.body);
     // console.log(flat);
-    return res.status(201).send({ flat });
+    return res.status(201).send(flat);
   } catch (e) {
     return res.status(500).json({ status: "Failed", message: e.message });
   }
@@ -22,7 +22,7 @@ router.patch("/:id", async (req, res) => {
       .lean()
       .exec();
 
-    return res.status(201).send({ flat });
+    return res.status(201).send(flat);
   } catch (e) {
     return res.status(500).json({ status: "Failed", message: e.message });
   }
@@ -52,68 +52,68 @@ router.get("/", async (req, res) => {
 
 //`http://localhost:4500/flat/${filter}/${value}/?page=${page}&size=${4}`
 
-router.get("/:filter/:sort", async (req, res) => {
-  try {
-    const page = +req.query.page || 1;
-    const size = +req.query.size || 4;
-    const skip = (page - 1) * size;
-    const filter = req.params.filter;
-    const sort = req.params.sort;
+// router.get("/:filter/:sort", async (req, res) => {
+//   try {
+//     const page = +req.query.page || 1;
+//     const size = +req.query.size || 4;
+//     const skip = (page - 1) * size;
+//     const filter = req.params.filter;
+//     const sort = req.params.sort;
 
-    if (filter == 0 && sort == 0) {
-      let flats = await Flat.find()
-        .populate("residents")
-        .skip(skip)
-        .limit(size)
-        .lean()
-        .exec();
+//     if (filter == 0 && sort == 0) {
+//       let flats = await Flat.find()
+//         .populate("residents")
+//         .skip(skip)
+//         .limit(size)
+//         .lean()
+//         .exec();
 
-      let totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
+//       let totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
 
-      return res.status(201).send({ flats, totalPage });
-    } else if (filter != 0 && sort == 0) {
-      let flats = await Flat.find({ type: filter })
-        .skip(skip)
-        .limit(size)
-        .populate("residents")
-        .lean()
-        .exec();
+//       return res.status(201).send({ flats, totalPage });
+//     } else if (filter != 0 && sort == 0) {
+//       let flats = await Flat.find({ type: filter })
+//         .skip(skip)
+//         .limit(size)
+//         .populate("residents")
+//         .lean()
+//         .exec();
 
-      let totalPage = Math.ceil(
-        (await Flat.find({ type: filter }).countDocuments()) / size
-      );
+//       let totalPage = Math.ceil(
+//         (await Flat.find({ type: filter }).countDocuments()) / size
+//       );
 
-      return res.status(201).send({ flats, totalPage });
-    } else if (filter != 0 && sort != 0) {
-      let flats = await Flat.find({ type: filter })
-        .sort({ flat_number: +sort })
-        .skip(skip)
-        .limit(size)
-        .populate("residents")
-        .lean()
-        .exec();
+//       return res.status(201).send({ flats, totalPage });
+//     } else if (filter != 0 && sort != 0) {
+//       let flats = await Flat.find({ type: filter })
+//         .sort({ flat_number: +sort })
+//         .skip(skip)
+//         .limit(size)
+//         .populate("residents")
+//         .lean()
+//         .exec();
 
-      let totalPage = Math.ceil(
-        (await Flat.find({ type: filter }).countDocuments()) / size
-      );
+//       let totalPage = Math.ceil(
+//         (await Flat.find({ type: filter }).countDocuments()) / size
+//       );
 
-      return res.status(201).send({ flats, totalPage });
-    } else if (filter == 0 && sort != 0) {
-      let flats = await Flat.find()
-        .sort({ flat_number: +sort })
-        .skip(skip)
-        .limit(size)
-        .populate("residents")
-        .lean()
-        .exec();
+//       return res.status(201).send({ flats, totalPage });
+//     } else if (filter == 0 && sort != 0) {
+//       let flats = await Flat.find()
+//         .sort({ flat_number: +sort })
+//         .skip(skip)
+//         .limit(size)
+//         .populate("residents")
+//         .lean()
+//         .exec();
 
-      let totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
+//       let totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
 
-      return res.status(201).send({ flats, totalPage });
-    }
-  } catch (e) {
-    return res.status(500).json({ status: "Failed", message: e.message });
-  }
-});
+//       return res.status(201).send({ flats, totalPage });
+//     }
+//   } catch (e) {
+//     return res.status(500).json({ status: "Failed", message: e.message });
+//   }
+// });
 
 module.exports = router;
