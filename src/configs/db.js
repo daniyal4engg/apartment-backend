1ac21ba7b71c4ebfb9ae7abc1c4a1cb85cd081e1
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-07-08 23:17:02 +0530

    flat controller added

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(
-    "mongodb+srv://daniyal:daniyal@foodapp.hm74x.mongodb.net/?retryWrites=true&w=majority"
-  );
+  return mongoose
+    .connect(
+      "mongodb+srv://daniyal:daniyal@foodapp.hm74x.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+      console.log("mongodb is connected");
+    })
+    .catch((error) => {
+      console.log("mondb not connected");
+      console.log(error);
+    });
 };
