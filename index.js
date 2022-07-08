c3f3a49d29aad46dc8195e360803d6daf4f67eb0
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-07-08 20:26:59 +0530

    authentication added

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-const express = require('express');
+const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const userController = require('./src/controllers/user.controller');
+const userController = require("./src/controllers/user.controller");
 
-const productController = require('./src/controllers/product.controller');
+const productController = require("./src/controllers/product.controller");
 
-const connect = require('./src/configs/db');
+const connect = require("./src/configs/db");
 
-const {register, login, email} = require('./src/controllers/auth.controller');
- 
+const { register, login, email } = require("./src/controllers/auth.controller");
+
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
 //register
-app.post('/register', register)
+app.post("/register", register);
 
 //login
-app.post("/login", login)
+app.post("/login", login);
 app.post("/email", email);
 
-
-
-
-app.use('/users', userController);
+app.use("/users", userController);
 app.use("/product", productController);
 
-
-app.listen(process.env.PORT || 5000, async() => {
-    try {
-        await connect();
-        console.log('Rocking On Port 5000');
-    }
-    catch(err){
-        console.log(err.message);
-    }
+app.listen(process.env.PORT || 5000, async () => {
+  try {
+    await connect();
+    console.log("Rocking On Port 5000");
+  } catch (err) {
+    console.log(err.message);
+  }
 });
--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    return mongoose.connect("mongodb+srv://zaki:zaki1234@authentication.ve9w8n4.mongodb.net/?retryWrites=true&w=majority")
+  return mongoose.connect(
+    "mongodb+srv://daniyal:daniyal@foodapp.hm74x.mongodb.net/?retryWrites=true&w=majority"
+  );
 };
